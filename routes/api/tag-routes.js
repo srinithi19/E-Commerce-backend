e6f63b72cfdd566da85539b4304d3ed3815d361d
Srinithi Ravichandran <emailtonithi@gmail.com>
2023-02-03 14:08:45 -0800

    add delete post api

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = await Category.findbyPk(req.params.id,{
+    const tagData = await Tag.findbyPk(req.params.id,{
       include: [{ model: Product }],
     });
     if (!tagData) {
@@ -41,8 +41,20 @@ router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: { id: req.params.id },
+    });
+    if (!tagData) {
+      res.status(404).json({ message: "No Tag found with that id!" });
+      return;
+    }
+    res.status(200).json(`Deleted ${req.params.id} Successfully!`);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
