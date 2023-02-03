2a8799fa03dca722d97c324c0319e1ff2cc80981
Srinithi Ravichandran <emailtonithi@gmail.com>
2023-02-03 14:06:04 -0800

    add tag post api

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -39,7 +39,7 @@ router.post('/', async (req, res) => {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
 
