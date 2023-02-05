0e64182e3609bcd212618101045b74f0e74f2fd5
Srinithi Ravichandran <emailtonithi@gmail.com>
2023-02-05 11:29:42 -0800

    add product routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -35,28 +35,12 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new tag
-  try {
-    const tagData = await Tag.create(req.body);
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
+  
 });
 
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
-  try {
-    const tagData = await Tag.update(req.body, {
-      where: { id: req.params.id },
-    });
-    if (!tagData) {
-      res.status(404).json({ message: "No Tag found with that id!" });
-      return;
-    }
-    res.status(200).json(`Updated ${req.params.id} Successfully!`);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  
 });
 
 router.delete('/:id', async (req, res) => {
